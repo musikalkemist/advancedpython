47bfed61f8f01ce0903ba982e30a399b3bb033de
Valerio Velardo <velardovalerio@gmail.com>
2022-04-07 18:10:29 +0200

    Implemented exercise for lesson 06

---
+++ src/06 Post Initialisation in Data Classes/exercise.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from typing import Union, List
+from dataclasses import dataclass, field
+
+
+@dataclass
+class EmployeeDTO:
+    first_name: str
+    last_name: str
+    work_email: str
+    mobile_number: int
+    managers: Union[List[str], List[EmployeeDTO]]
+    complete_name: str = field(init=False)
+
+    def __post_init__(self):
+        self.complete_name = f"{self.first_name} {self.last_name}"
+
+    @classmethod
+    def from_dict(self, args_dict: dict) -> EmployeeDTO:
+        return EmployeeDTO(**args_dict)
+
+
+if __name__ == "__main__":
+    args_dict = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "work_email": "john@company.com",
+        "mobile_number": 12345,
+        "managers": ["Max", "Frodo"]
+    }
+    employee = EmployeeDTO.from_dict(args_dict)
+    print(employee.complete_name)
